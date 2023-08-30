cca722b23d90a642810e4d096e6075cf14adb4c1
Caio Leitão <contato@caioleitao.com.br>
2023-08-30 14:42:25 -0300

    ädicionei projeto no xcode, e fiz o programa iniciar via raylib com background, porem nao funcionando ainda

---
+++ jogodanavinha/main.c
@@ -0,0 +1,26 @@
+#include "raylib.h"
+#include "stdio.h"
+
+int main(){
+	const int screenWidth = 1280;
+	const int screenHeight = 720;
+
+	InitWindow(screenWidth, screenHeight, "Jogo de navinha");
+
+	Texture2D background = LoadTexture("background.jpg");
+
+	while(!WindowShouldClose()){
+		BeginDrawing();
+
+		ClearBackground(RAYWHITE);
+
+		DrawTexture(background, 0, 0, BLACK);
+
+		EndDrawing();
+	}	
+
+	UnloadTexture(background);
+	CloseWindow();
+	
+	return 0;
+}
